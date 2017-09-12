afd667847ee9c4d85da0dac8f05c45c40f6cf7cf
Nick <nickmour5@gmail.com>
2017-09-12 12:01:07 +0300

    Fixed files extensions

--- vitadb.sh
+++ vitadb.sh
@@ -1,45 +1,54 @@
 #!/bin/bash
+clear
 echo "Choose your action"
 echo -e "Type: \n'h' for Homebrews \n'p' for Plugins"
 read a
 if [ $a == h ]; then
   n=$(curl -s https://rinnegatamante.it/vitadb/list_hbs_json.php | jq -r 'keys' | tail -n 2 | sed -n 1p)
-  echo "Currenty in the db there are" $n "Homebrews"
+  echo "Currenty in the db there are" $n "Homebrews, counting from 0"
   echo "Your last Homebrew was :"
-  cat log.txt | head -n 1
+  head -n 1 log.txt
   echo -e "Where do you want the dl to stop? \nSTART COUNTING FROM 0!!"
-  echo "Waiting for input"
+  echo "Waiting for input..."
   read x
-  mkdir hb
-  curl -s https://rinnegatamante.it/vitadb/list_hbs_json.php >> hb/db.json
+  mkdir -p hb && cd hb
+  curl -s https://rinnegatamante.it/vitadb/list_hbs_json.php >> db.json
   for (( i=0; i<=$x; i++ )); do
-    filename=$(jq --arg i $i -r '.[$i | tonumber] | "\(.name)_\(.version)"' hb/db.json)
-    data=$(jq --arg i $i -r '.[$i | tonumber] | "\(.data)"' hb/db.json)
+    filename=$(jq --arg i $i -r '.[$i | tonumber] | "\(.name)_\(.version)"' db.json)
+    data=$(jq --arg i $i -r '.[$i | tonumber] | "\(.data)"' db.json)
     if [ "$data" != "" ]; then
       wget -O data${filename// /_} $data
     fi
-    jq --arg i $i -r '.[$i | tonumber] | "\(.url)"' hb/db.json | xargs wget -O ${filename// /_}
+    jq --arg i $i -r '.[$i | tonumber] | "\(.url)"' db.json | xargs wget -O ${filename// /_}
   done
-  jq -r '.[0] | "\(.name)_\(.version)"' hb/db.json > log.txt
-  rm hb/db.json
+  jq -r '.[0] | "\(.name)_\(.version)"' db.json > ../log.txt
+  sed -i '1a\Nothing' log.txt
+  rm db.json
 elif [ $a == p ]; then
   n=$(curl -s https://rinnegatamante.it/vitadb/list_plugins_json.php | jq -r 'keys' | tail -n 2 | sed -n 1p)
   echo "Currenty in the db there are" $n "Plugins"
   echo "Your last Plugin was :"
-  cat log.txt | tail -n 1
+  tail -n 1 log.txt
   sed -i '$ d' log.txt
   echo -e "Where do you want the dl to stop? \nSTART COUNTING FROM 0"
-  echo "Waiting for input"
+  echo "Waiting for input..."
   read x
-  mkdir pg
-  curl -s https://rinnegatamante.it/vitadb/list_plugins_json.php >> pg/db.json
+  curl -s https://rinnegatamante.it/vitadb/list_plugins_json.php >> db.json
+  mkdir -p pg && cd pg
   for (( i=0; i<=$x; i++ )); do
-    filename=$(jq -r '.[$i | tonumber] | "\(.name)_\(.version)"' pg/db.json)
-    jq -r '.[$i | tonumber] | "\(.url)"' pg/db.json | xargs wget -O ${filename// /_}
+    filename=$(jq --arg i $i -r '.[$i | tonumber] | "\(.name)_\(.version)"' ../db.json)
+    url=$(jq --arg i $i -r '.[$i | tonumber] | "\(.url)"' ../db.json)
+    ext=$(curl -sI $url | grep file_path)
+    ext=${ext##*.}
+    wget -O ${filename// /_}.$ext $url
   done
-  jq -r '.[0] | "\(.name)_\(.version)"' pg/db.json >> log.txt
-  rm pg/db.json
+  for f in *; do
+    mv "$f" "${f%?}"
+  done
+  echo "Nothing" >> ../log.txt
+  jq -r '.[0] | "\(.name)_\(.version)"' ../db.json >> ../log.txt
+  rm ../db.json
 else
   echo "EXITING"
-  exit;
+  exit
 fi
