734bfbb4d30e397073f373e84bb7fc2bee80fd19
Nick <nickmour5@gmail.com>
2017-09-13 13:41:04 +0300

    Added .vpk extensions

--- vitadb.sh
+++ vitadb.sh
@@ -11,19 +11,26 @@ if [ $a == h ]; then
   echo -e "Where do you want the dl to stop? \nSTART COUNTING FROM 0!!"
   echo "Waiting for input..."
   read x
-  mkdir -p hb && cd hb
   curl -s https://rinnegatamante.it/vitadb/list_hbs_json.php >> db.json
+  mkdir -p hb && cd hb
   for (( i=0; i<=$x; i++ )); do
-    filename=$(jq --arg i $i -r '.[$i | tonumber] | "\(.name)_\(.version)"' db.json)
-    data=$(jq --arg i $i -r '.[$i | tonumber] | "\(.data)"' db.json)
+    filename=$(jq --arg i $i -r '.[$i | tonumber] | "\(.name)_\(.version)"' ../db.json)
+    data=$(jq --arg i $i -r '.[$i | tonumber] | "\(.data)"' ../db.json)
     if [ "$data" != "" ]; then
       wget -O data${filename// /_} $data
     fi
-    jq --arg i $i -r '.[$i | tonumber] | "\(.url)"' db.json | xargs wget -O ${filename// /_}
+    jq --arg i $i -r '.[$i | tonumber] | "\(.url)"' ../db.json | xargs wget -O ${filename// /_}
   done
-  jq -r '.[0] | "\(.name)_\(.version)"' db.json > ../log.txt
-  sed -i '1a\Nothing' log.txt
-  rm db.json
+  for f in *; do
+    if [[ $f != data* ]]; then
+      mv -f $f $f.vpk
+    else
+      mv -f $f $f.zip
+    fi
+  done
+  jq -r '.[0] | "\(.name)_\(.version)"' ../db.json > ../log.txt
+  sed -i '1a\Nothing' ../log.txt
+  rm ../db.json
 elif [ $a == p ]; then
   n=$(curl -s https://rinnegatamante.it/vitadb/list_plugins_json.php | jq -r 'keys' | tail -n 2 | sed -n 1p)
   echo "Currenty in the db there are" $n "Plugins"
